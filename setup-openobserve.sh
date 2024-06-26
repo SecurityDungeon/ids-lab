0bb2b3d768cfe60ace8d5d0d9dfe57cd2b7c5898
Ladislav Baco <lacike@users.noreply.github.com>
2024-06-26 08:55:09 +0200

    Fix setup-openobserve script

--- setup-openobserve.sh
+++ setup-openobserve.sh
@@ -9,42 +9,49 @@ curl -X 'POST' \
    -H 'accept: application/json' \
    -H 'Content-Type: application/json' \
    -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
    --data-binary '@openobserve/WordPress.view.json'
 curl -X 'POST' \
    'http://127.0.0.1:5080/api/lab/savedviews' \
    -H 'accept: application/json' \
    -H 'Content-Type: application/json' \
    -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
    --data-binary '@openobserve/SuricataAlerts.view.json'
 curl -X 'POST' \
    'http://127.0.0.1:5080/api/lab/savedviews' \
    -H 'accept: application/json' \
    -H 'Content-Type: application/json' \
    -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
    --data-binary '@openobserve/SuricataFlows.view.json'
 curl -X 'POST' \
    'http://127.0.0.1:5080/api/admin/savedviews' \
    -H 'accept: application/json' \
    -H 'Content-Type: application/json' \
    -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
    --data-binary '@openobserve/WordPress.view.json'
 curl -X 'POST' \
    'http://127.0.0.1:5080/api/admin/savedviews' \
    -H 'accept: application/json' \
    -H 'Content-Type: application/json' \
    -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
    --data-binary '@openobserve/SuricataAlerts.view.json'
 curl -X 'POST' \
    'http://127.0.0.1:5080/api/admin/savedviews' \
    -H 'accept: application/json' \
    -H 'Content-Type: application/json' \
    -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
    --data-binary '@openobserve/SuricataFlows.view.json'
 curl -X 'POST' \
    'http://127.0.0.1:5080/api/admin/savedviews' \
    -H 'accept: application/json' \
    -H 'Content-Type: application/json' \
-   -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD}
+   -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
    --data-binary '@openobserve/Docker.view.json'
 
 echo "Create OpenObserve dashboards..."
@@ -53,10 +60,12 @@ curl -X 'POST' \
    -H 'accept: application/json' \
    -H 'Content-Type: application/json' \
    -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
    --data-binary '@openobserve/LabDashboard.dashboard.json'
 curl -X 'POST' \
    'http://127.0.0.1:5080/api/admin/dashboards' \
    -H 'accept: application/json' \
    -H 'Content-Type: application/json' \
    -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
    --data-binary '@openobserve/LabDashboard.dashboard.json'
