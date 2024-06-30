37d99285045fa21ca4b008c3f4594de28d1b1a5f
Ladislav Baco <lacike@users.noreply.github.com>
2024-06-30 17:43:02 +0200

    Add Windows logs saved views and functions

--- setup-openobserve.sh
+++ setup-openobserve.sh
@@ -3,6 +3,15 @@
 
 source .env
 
+echo "Create OpenObserve functions..."
+curl -X 'POST' \
+   'http://127.0.0.1:5080/api/lab/functions' \
+   -H 'accept: application/json' \
+   -H 'Content-Type: application/json' \
+   -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
+   --data-binary '@openobserve/ParseWinEvtLog.function.json'
+
 echo "Create OpenObserve saved views..."
 curl -X 'POST' \
    'http://127.0.0.1:5080/api/lab/savedviews' \
@@ -25,6 +34,28 @@ curl -X 'POST' \
    -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
    --output /dev/null \
    --data-binary '@openobserve/SuricataFlows.view.json'
+curl -X 'POST' \
+   'http://127.0.0.1:5080/api/lab/savedviews' \
+   -H 'accept: application/json' \
+   -H 'Content-Type: application/json' \
+   -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
+   --data-binary '@openobserve/WindowsEventLog.view.json'
+curl -X 'POST' \
+   'http://127.0.0.1:5080/api/lab/savedviews' \
+   -H 'accept: application/json' \
+   -H 'Content-Type: application/json' \
+   -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
+   --data-binary '@openobserve/WindowsLogon.view.json'
+curl -X 'POST' \
+   'http://127.0.0.1:5080/api/lab/savedviews' \
+   -H 'accept: application/json' \
+   -H 'Content-Type: application/json' \
+   -u ${ZO_ROOT_USER_EMAIL}:${ZO_ROOT_USER_PASSWORD} \
+   --output /dev/null \
+   --data-binary '@openobserve/WindowsProcess.view.json'
+
 curl -X 'POST' \
    'http://127.0.0.1:5080/api/admin/savedviews' \
    -H 'accept: application/json' \
